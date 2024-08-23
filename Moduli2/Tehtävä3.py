0969ab84ea2ebc7b864c9cb1f8728f0729084ae5
nasseron <Nassero.Nassero@metrpolia.fi>
2024-08-23 13:51:48 +0200

    visit

--- Moduli2/Tehtävä3.py
+++ Moduli2/Tehtävä3.py
@@ -3,3 +3,4 @@ korkea = int(input("Mikä on suorakulmion korkea?"))
 pinta_ala = kaanta * korkea
 piiri = kaanta * 2 + korkea * 2
 print("suorakulmion pinta_ala on:", pinta_ala, "suorakulmion piiri on:",piiri)
+#Läksyt
