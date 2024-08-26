25e106e84268ca102f3e25e29459435b82c25558
nasseron <Nassero.Nassero@metrpolia.fi>
2024-08-26 12:53:25 +0200

    visit

---
+++ Moduli4/Tehtävä2.py
@@ -0,0 +1,9 @@
+
+tuumia = float(input("Montako tuumia muunetaan: "))
+tuumi = 2.45
+while tuumia <0:
+    maara = tuumia / tuumi
+    print(tuumia)
+    tuumia +=1
+    if tuumia > 0:
+        break
